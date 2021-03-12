40f9f4ace7c3f3ac36643b1a012ceb1ca0a97ffe
Adis012 <adis012@hotmail.com>
2021-03-12 18:13:53 +0100

    adding index

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,15 @@
+function main(){
+    // Only change code belowe this line
+    var myFirstString = " I think there I \"am\" -- sentient. ";
+    var mySecondString = " No, you only \"think\" , so you aren't. ";
+    // Only change code above this line
 
+    return {
+        mvFirstString,
+        mySecondStringe
+
+    };
+}
+
+console.log(mein));
+module.exports = main;

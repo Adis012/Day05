82053144368e9b9360cb9de362404e79fdf843c6
Adis012 <adis012@hotmail.com>
2021-03-12 20:57:50 +0100

    adding index

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // Only change code belowe this line 
+    var lastName = "Doe";
+    var firstLetterOfLastName = lastName[0]; // firstLetter is "D"
+
+    // Only change code above this line
+    return firstLetterOfLastName;
+
+}
+
+console.log(main));
+module.exports = main;
+

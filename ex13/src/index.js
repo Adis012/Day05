ad0162a42a02a0d58aeca2f158b3640c2eb52f1a
Adis012 <adis012@hotmail.com>
2021-03-12 21:45:53 +0100

    adding index

---
+++ ex13/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code below this line
+    var lastName = "Bezos"
+    var secondToLastLetter = lastName[lastName.length - 2]; // secondToLastLetter is "z"
+
+    // Only change code above this line
+    return thirdToLastLetterOfLastName;
+
+}
+
+console.log(main));
+module.exports = main;

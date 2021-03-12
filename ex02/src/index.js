93b05a94a289a9b179229d1d520dd35640575f1b
Adis012 <adis012@hotmail.com>
2021-03-12 18:26:04 +0100

    adding index

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    var myLink = "<a href=\"http://www.example.com\" target=\"-_blank\">
+    // Only change code above this line 
+
+    singleQuote = '\"http://www.example.com\';
+    doubleQuote = "\"target=\";
+
+    return myLink;
+
+
+}
+
+console.log(main));
+module.exports = main;

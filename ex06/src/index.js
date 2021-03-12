2b7909a233959251c5d1d21a97933155f12a4e2e
Adis012 <adis012@hotmail.com>
2021-03-12 19:58:20 +0100

    adding index

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    // Only change code belowe this line
+    var myName = "adis";
+    var myString = "My name is" + "Adis" + ", and I am well!"
+
+    // Only changecode above this line
+    return myString;
+    
+}
+
+console.log(main));
+module.exports = main;
+
+    
+     
+
+
+
+    

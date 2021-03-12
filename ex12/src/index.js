9555257b9284a1ba16e709cda7725df5036deb17
Adis012 <adis012@hotmail.com>
2021-03-12 21:37:18 +0100

    adding index

---
+++ ex12/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // Only change code belowe this line
+    var lastName = "Tesla";
+    var lastLetter = lastName[lastName.length - 1]; // lastLetter is "a"
+    // Only change code above this line
+    return lastLetterOfLastName;
+
+}
+
+console.log());
+module.exports = main;

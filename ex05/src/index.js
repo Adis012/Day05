70f58e429a2c083127f6acf6238c11d4aad75968
Adis012 <adis012@hotmail.com>
2021-03-12 19:42:50 +0100

    adding index

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code below this line
+    var myString = "Concatenation is the process of appending one string to the end of another string."
+    myString += "One string to the end of another string."
+
+    // Only change code above this line
+    return myString;
+
+}
+
+console.log(main));
+module.exports = main;

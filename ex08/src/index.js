04e0bbcc1b57e59e6e61ef45510f9592a87d1e96
Adis012 <adis012@hotmail.com>
2021-03-12 20:45:56 +0100

    adding index

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code belowe this line
+    var lastname = "Doe";
+    var lastNameLength;
+
+    // Only change code above this line
+    return lastNameLength;
+
+}
+
+console.log(main));
+module.exports = main;

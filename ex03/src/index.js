cf1b2eab2fe0a41c1a3876cc57cc2fbb3f45e775
Adis012 <adis012@hotmail.com>
2021-03-12 19:14:17 +0100

    adding index

---
+++ ex03/src/index.js
@@ -0,0 +1,25 @@
+function main(){
+    // Only change code below this line
+    var myString = "FirstLine\n SecondLine\t Thirdline\";
+ 
+    // Only change code above this line
+
+    return myString;
+
+    Firstline = \n,
+    Secondline = \t,
+    Thirdline = \;
+
+}
+
+console.log(main));
+module.exports = main;
+
+    
+    
+
+
+
+
+
+
